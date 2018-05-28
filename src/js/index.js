a6da800cf6c881244ef47797fedecbdac2559520
dominickenorwood <dominickenorwood@gamil.com>
2018-05-27 23:57:43 -0400

    Started work on touch events

--- src/js/index.js
+++ src/js/index.js
@@ -11,3 +11,53 @@ const messenger = new InfiniteMessenger({
 });
 
 console.log(messenger);
+
+const dock = document.createElement('div');
+dock.setAttribute('id', 'dock');
+const body = document.getElementsByTagName('body');
+body[0].appendChild(dock);
+
+// https://gomakethings.com/how-to-get-the-closest-parent-element-with-a-matching-selector-using-vanilla-javascript/
+const getClosest = (elem, selector) => {
+
+    // Get the closest matching element
+    for( ; elem && elem !== document; elem = elem.parentNode){ 
+
+        if(elem.matches( selector )) {
+            return elem;
+        }
+    }
+    return null;
+}
+
+const root = document.getElementById('app-messenger');
+root.addEventListener('touchstart', (event) => {
+    const article = getClosest(event.target, '.message');
+
+    if(article){
+        article.classList.add('u-touched');
+        console.log('[Touch Start]', article);
+        dock.innerHTML =  `Start At: ${Math.floor(event.touches[0].pageX)}`;
+    }
+})
+
+root.addEventListener('touchend', (event) => {
+    const article = getClosest(event.target, '.message');
+    if(article) {
+        article.classList.remove('u-touched');
+        console.log('[Touch End]', article);
+        dock.innerHTML =  `End At: ${Math.floor(event.changedTouches[0].pageX)}`;
+    }
+})
+
+root.addEventListener('touchmove', (event) => {
+    const article = getClosest(event.target, '.message');
+    if(article) {
+        //console.log('[Touch Move Event]', event)
+        console.log('[Touch Move]', article);
+        dock.innerHTML =  `Move: ${Math.floor(event.touches[0].pageX)}`;
+    }
+})
+
+
+console.log(root);
