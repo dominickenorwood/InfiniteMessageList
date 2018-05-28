020b35ef1249e95f6494a9b3833ac41129909eab
dominickenorwood <dominickenorwood@gamil.com>
2018-05-28 01:32:18 -0400

    Finished rough of touch delete event

--- src/js/index.js
+++ src/js/index.js
@@ -12,10 +12,10 @@ const messenger = new InfiniteMessenger({
 
 console.log(messenger);
 
-const dock = document.createElement('div');
-dock.setAttribute('id', 'dock');
-const body = document.getElementsByTagName('body');
-body[0].appendChild(dock);
+// const dock = document.createElement('div');
+// dock.setAttribute('id', 'dock');
+// const body = document.getElementsByTagName('body');
+// body[0].appendChild(dock);
 
 // https://gomakethings.com/how-to-get-the-closest-parent-element-with-a-matching-selector-using-vanilla-javascript/
 const getClosest = (elem, selector) => {
@@ -30,34 +30,60 @@ const getClosest = (elem, selector) => {
     return null;
 }
 
+let move = 0;
+let articleX = 0;
+
 const root = document.getElementById('app-messenger');
 root.addEventListener('touchstart', (event) => {
     const article = getClosest(event.target, '.message');
 
     if(article){
+        move = 0;
+        articleX = article.getBoundingClientRect().left;
         article.classList.add('u-touched');
-        console.log('[Touch Start]', article);
-        dock.innerHTML =  `Start At: ${Math.floor(event.touches[0].pageX)}`;
+        //console.log('[Touch Start]', articleX);
+        //dock.innerHTML =  `Start At: ${Math.floor(event.touches[0].pageX)}`;
+    }
+})
+
+
+root.addEventListener('touchmove', (event) => {
+    const article = getClosest(event.target, '.message');
+    if(article) {
+        move += 5;
+        //console.log('[Touch Move Event]', event)
+        article.style.transform = `translate3d(${articleX + move}px,0,0)`;
+        //console.log('[Touch Move]', article);
+        //dock.innerHTML =  `Move: ${Math.floor(event.touches[0].pageX)}, count: ${move}`;
     }
 })
 
 root.addEventListener('touchend', (event) => {
     const article = getClosest(event.target, '.message');
     if(article) {
+        let deleteArticle = false;
         article.classList.remove('u-touched');
-        console.log('[Touch End]', article);
-        dock.innerHTML =  `End At: ${Math.floor(event.changedTouches[0].pageX)}`;
+        //console.log('[Touch End]', article);
+        //dock.innerHTML =  `End At: ${Math.floor(event.changedTouches[0].pageX)}`;
+        if(Math.floor(article.getBoundingClientRect().left) > 50) {
+            if(!deleteArticle){
+                deleteArticle = true;
+                article.style.transform = `translate3d(100vw,0,0)`;
+                article.style.height = `${article.offsetHeight}px`
+                article.addEventListener('transitionend', (event) => {
+                    if(event.propertyName === 'transform'){
+                        //console.log('transform ended');
+                        article.style.height = '0';
+                        article.style.margin = '0';
+                        article.style.padding = '0';
+                    }
+                });
+            }
+        } else {
+            article.style.transform = `translate3d(0,0,0)`;
+        }
     }
 })
 
-root.addEventListener('touchmove', (event) => {
-    const article = getClosest(event.target, '.message');
-    if(article) {
-        //console.log('[Touch Move Event]', event)
-        console.log('[Touch Move]', article);
-        dock.innerHTML =  `Move: ${Math.floor(event.touches[0].pageX)}`;
-    }
-})
+//console.log(root);
 
-
-console.log(root);
