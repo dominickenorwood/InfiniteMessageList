8f73b472f8b7091f90a1808413b28356da375837
dominickenorwood <dominickenorwood@gamil.com>
2018-05-29 13:33:15 -0400

    throw away files

--- src/js/components/API/Payload.js
+++ src/js/components/API/Payload.js
@@ -3,6 +3,7 @@ import { fetchData } from './Fetch';
 export const getPayload = async (url) => {
     try {
         const payload = await fetchData(url);
+        console.log(payload);
         return payload;
 
     } catch(error) {
